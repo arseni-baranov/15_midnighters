b64cd0b75ca5e237c9e2dc970a82cc2ee9df9e51
coding@justlike.pro <coding@justlike.pro>
2016-12-17 22:02:50 +0500

    all done

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,36 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    devman_api = "https://devman.org/api/challenges/solution_attempts?page={page}"
+    number_of_pages = requests.get(devman_api.format(page=1)).json()['number_of_pages']
+
+    for page in range(1, number_of_pages):
+        response = requests.get(devman_api.format(page=page)).json()
+        for entry in response['records']:
+            yield entry
+
+
+def get_midnighters(records):
+    for entry in records:
+        dt = get_datetime(entry['timestamp'], entry['timezone'])
+        if dt and dt.hour in range(1, 5):
+            yield entry['username'], dt
+
+
+def get_datetime(timestamp, timezone):
+    if timestamp is None or timezone is None:
+        return None
+    return datetime.fromtimestamp(timestamp, pytz.timezone(timezone))
+
+
+def main():
+    for user, dt in get_midnighters(load_attempts()):
+        print('"{0}" sent task on {1} at {2}'.format(user, dt.strftime('%d-%m-%Y'), dt.strftime('%H:%M %Z')))
+
 
 if __name__ == '__main__':
-  pass
+  main()
+
